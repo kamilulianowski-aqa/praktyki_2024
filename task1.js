0389569844ac26c822f28e4d9550c083f8fdb4e2
kamilulianowski <kamil.ulianowski@flip.shop>
2024-09-23 18:09:07 +0200

    new task

---
+++ task1.js
@@ -0,0 +1,9 @@
+/* 
+
+1. Zainstaluj Postmana na komputerze
+2. Zapoznaj się z API Narodowego Banku Polskiego https://api.nbp.pl/#kursyParams
+3. Wyykonaj w Postmanie request https://api.nbp.pl/api/exchangerates/tables/A
+4. Dowiedz sie czym jest curl, jak mona go uzywac, następnie wklej curl jako komentarz tutaj
+5. Poczytaj o async / await, przyda się w kolejnym zadaniu
+6. W javascript z uzyciem async / await wykonaj request do https://api.nbp.pl/api/exchangerates/tables/A i wypisz aktualny kurs walut USD, EUR, GBP
+7. Wykonaj request do api który sprawdzi aktualną cenę złota, następnie jako console.log  wypisz ją
